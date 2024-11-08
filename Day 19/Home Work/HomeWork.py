bf31d88addeea4de8857110cb4575d97250ea6b9
Zukasheucherebel1 <khorguanizura630@gmail.com>
2024-11-08 19:34:38 +0400

    Commit

--- Day 19/Home Work/HomeWork.py
+++ Day 19/Home Work/HomeWork.py
@@ -1,23 +1,51 @@
-numbers = [3, 8, 15, 22, 5, 10, 12, 7, 6, 19]
+
+items = ["apple", "banana", "cherry", "date", "elderberry"]
+items = items[:-2]  
+print(items)
 
 
-sum_even = 0
-sum_odd = 0
+my_string = "Hello, world!"
+print(my_string[::-1])
 
 
-i = 0
+numbers = [10, 23, 5, 67, 12, 78, 34, 89, 2, 45]
+smallest = min(numbers)
+largest = max(numbers)
+sum_of_smallest_and_largest = smallest + largest
+print(sum_of_smallest_and_largest)
 
 
-while i < len(numbers):
-    if numbers[i] % 2 == 0:  #ლუწი
-        sum_even += numbers[i]
-    else:  #კენტი
-        sum_odd += numbers[i]
-    i += 1
 
-if sum_even > sum_odd:
-    print("The sum of even numbers is greater:", sum_even)
-elif sum_odd > sum_even:
-    print("The sum of odd numbers is greater:", sum_odd)
-else:
-    print("Both sums are equal:", sum_even)
+
+my_string = "madam"
+
+print("Is palindrome:", 'Is_palidrome')
+
+
+numbers = [10, 23, 5, 67, 12, 78, 34, 89, 2, 45]
+even_count = 0
+odd_count = 0
+
+for number in numbers:
+    if number % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
+
+print("Even count:", even_count)
+print("Odd count:", odd_count)
+
+
+
+original_list = [1, 0, 1, 1, 0, 0, 1]
+new_list = []
+
+
+for value in original_list:
+    if value == 1:
+        new_list.append(True)
+    else:
+        new_list.append(False)
+
+
+print(new_list)

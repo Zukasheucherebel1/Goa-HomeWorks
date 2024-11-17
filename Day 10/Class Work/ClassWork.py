b6aac58fae76c6457a2ef317fcc42325f999c4e3
Zukasheucherebel1 <khorguanizura630@gmail.com>
2024-11-17 19:32:51 +0400

    Commit

---
+++ Day 10/Class Work/ClassWork.py
@@ -0,0 +1,14 @@
+
+asaki = int(input("enter your age: "))
+
+# ლუწი თუ კენტი ასაკის შემოწმება
+if asaki % 2 == 0:
+    print("your age is even.")
+else:
+    print("your age is odd.")
+
+
+for i in range(10, 51):
+    if i % 2 == 0:
+        print(i)
+
